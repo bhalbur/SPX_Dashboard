a1983605e9cdf67cbba537b294aa9c50d19397fd
Brian <brian.t.halbur@gmail.com>
2019-03-14 19:33:31 -0700

    progress 3/14

--- project_2/static/js/SPX_app.js
+++ project_2/static/js/SPX_app.js
@@ -1,3 +1,31 @@
+function historicAPI(){
+}
+
+function currentAPI(){
+
+}
+
+function basicInfo(currpx){
+  d3.select('api-div')
+  d3.html(currpx)
+}
+
+function buildMap(){
+
+var myMap = L.map("map-div", {
+  center: [37.0902, -95.7129],
+  zoom: 4,
+});
+
+var baseMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 10,
+  minZoom: 4,
+  id: "mapbox.streets",
+  accessToken: 'pk.eyJ1IjoiYmhhbGJ1ciIsImEiOiJjanN3bG45aHMwNjdrNDNwZmltcmhyMTU5In0.4fWTF1uFox70N-HNCUejHQ'
+}).addTo(myMap);
+
+}
 
 
 function init() {
@@ -16,9 +44,17 @@ function init() {
     });
 
     // Use the first sample from the list to build the initial plots
-    var defaultTicker = tickerList[Math.random(0,tickerList.length)];
+    var defaultTicker = tickerList[Math.floor(Math.random() * tickerList.length)];
     console.log(defaultTicker);
   });
 }
 
+
+function optionChanged(dropdown_value) {
+  d3.json(`/basicinfo/${dropdown_value}`).then(basicInfo);  
+  d3.json(`/currentprice/${dropdown_value}`).then(currentAPI);  
+  d3.json(`/historicprice/${dropdown_value}`).then(historicAPI);  
+}
+
+buildMap()
 init()
