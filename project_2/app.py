282f8ff5d78feca5e3213bac6b42f57167874661
adambrush10 <aebrush10@gmail.com>
2019-03-14 19:47:21 -0700

    updated app.py

--- project_2/app.py
+++ project_2/app.py
@@ -46,41 +46,18 @@ def scrape():
 
 
 
+
+
 @app.route("/currentprice/<ticker>")
 def currentprice(ticker):
-   ### LEFTOVERS FROM PREVIOUS HOMEWORK
-   ### LEFTOVERS FROM PREVIOUS HOMEWORK
-   ### LEFTOVERS FROM PREVIOUS HOMEWORK
+  symbol = ticker
+  response = requests.get(f'https://cloud.iexapis.com/beta/stock/{symbol1}/price/quote?token={API_KEY}')
+  response_json = response.json()
+  return(json.dumps(response_json, indent=4, sort_keys=True))
 
-##API CALL URL + ticker
 
 
-    """Return the MetaData for a given sample."""
-    sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
-    ]
 
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-    # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
-    for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
-
-    print(sample_metadata)
-    return jsonify(sample_metadata)
 
 
 @app.route("/samples/<sample>")
@@ -94,10 +71,10 @@ def samples(sample):
 
     # Filter the data based on the sample number and
     # only keep rows with values above 1
-    
+
     sample_data1 = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
     sample_data = sample_data1.sort_values(by=[sample])
-    
+
 
     # Format the data to send as json
     data = {
